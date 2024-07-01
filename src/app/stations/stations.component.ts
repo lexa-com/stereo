b85bdebbec3dda4104491a9a2f9b450e6166cbb4
Gitu J <mwathijoel@gmail.com>
2024-07-01 14:04:54 +0300

    dep

--- src/app/stations/stations.component.ts
+++ src/app/stations/stations.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { AudioService } from '../audio.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
-import { faCoffee,faMessage } from '@fortawesome/free-solid-svg-icons';
+import { faCoffee,faMessage,faHeart,faHome } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-stations',
@@ -12,6 +12,8 @@ import { faCoffee,faMessage } from '@fortawesome/free-solid-svg-icons';
 export class StationsComponent implements OnInit {
 
   telegram = faMessage
+  home = faHome
+  favourites = faHeart
   
   stations: any[] = []
   radio: any;
@@ -20,28 +22,48 @@ export class StationsComponent implements OnInit {
   pause = "assets/pause.png"
   isPlaying: boolean = false;
   @ViewChild('audio') audio: any;
+  stationss: any;
+  radiosArray:any[]=[]
 
   constructor(private stationsAPI: AudioService,
     private router:Router
   ) {}
 
-  ngOnInit(): void {
-    {
-      this.stationsAPI.getAudioStreamUrl().subscribe(res => {
-        this.stations = res;
-        localStorage.setItem('currentRadio', JSON.stringify(res));
-
-      });
-    }
+  ngOnInit(): void {    
+    this.getStations()
     this.togglePlayPause()
+    this.getFavourites()
   }
 
-  playRadio(radio:any){
-    this.cover = radio.coverurl
-    this.radio = radio.url
-    this.togglePlayPause()
-    // localStorage.setItem('currentRadio', JSON.stringify(radio));
-    //this.router.navigate(["/player"],{skipLocationChange :false, queryParams:{radio:radio}})
+  playRadio(radio: any) {
+    this.cover = radio.coverurl;
+    this.radio = radio.url;
+    this.togglePlayPause();
+  
+    const favourites = JSON.parse(localStorage.getItem('favourites') || '[]');
+    const idsArray = favourites.map((fav: { _id: any; }) => fav._id);
+    console.log(radio._id)
+    console.log(idsArray)
+    
+    if (idsArray.includes(radio._id)){
+      console.log("already exists")
+      
+    }
+    else{favourites.push(radio);}
+
+    
+    
+    localStorage.setItem('favourites', JSON.stringify(favourites));
+  
+  }
+  
+
+  getStations(){
+    this.stationsAPI.getAudioStreamUrl().subscribe(res => {
+      this.stations = res;
+     //localStorage.setItem('currentRadio', JSON.stringify(res));
+
+    })
   }
 
   togglePlayPause() {
@@ -51,6 +73,13 @@ export class StationsComponent implements OnInit {
       this.audio.nativeElement.play();
     }
     this.isPlaying = !this.isPlaying;
+  }
+
+  getFavourites(){
+    const storedStations = localStorage.getItem('favourites');
+    if (storedStations) {
+      this.stations = JSON.parse(storedStations);
+    }
   }
 
 
